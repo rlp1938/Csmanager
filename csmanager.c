bd8890f6e095e3e89453ca5411da2652e1cf3d29
bob <rlp1938@gmail.com>
2018-01-28 19:23:25 +0700

    Major refactor. **WARN** sync simple files in source dir not done yet.

--- csmanager.c
+++ csmanager.c
@@ -34,137 +34,78 @@
 #include <libgen.h>
 #include <errno.h>
 // typdefs/structs here.
-typedef struct ops_t {
-	char *dirname;		// the name of a dir or a file listing dirs.
+typedef struct oper_t {
+	char *dirname;		// source dir to be synced.
+	char *filname;		// file listing source dirs to sync.
 	char *cloud_target;	// eg Dropbox or Nextcloud;
-	int do_master_dir;	// a dir, not a file listing dirs.
-	int do_dot_dirs;	// 0 is avoid dot dirs, 1 is dot dirs only.
-	time_t dd_updated;	// last time we processed dot dirs.
-	char *dotfiles_dir;	// the dir to send dot files tarballs to.
-	char *timestamp;	// path to the timestamp for this process.
-	char **files_list;	// list of dirs to process.
+	char *dotdirs_dir;	// the dir to send dot dir contents to.
 	char **excludes;	// list of dirs to exclude eg $HOME/Dropbox etc.
-} ops_t;
+	int do_master_dir;	// a dir, not a file listing dirs.
+	size_t len2target;	// byte count to (for example) $HOME/Nextcloud
+} oper_t;
 #include "str.h"
 #include "dirs.h"
 #include "files.h"
 #include "gopt.h"
-
-static int
-nc_recursedir(ops_t *operation, const char *dirname);
+static oper_t
+*init_operations(char *srcdir, options_t *opts);
 static char
-*timestamp_name(void);
-static void
-process_dotdirs(ops_t *operation);
-static void
-check_args(ops_t *ops, char **argv, int optind, options_t *opts);
-static char
-**make_dirslist(ops_t *operation);
+*check_args(char **argv);
 static char
 **gen_dirslist(const char *dirname, int dotsornot, char **excl_list);
 static char
-**gen_fileslist(const char *dirname, char **excl_list);
-static char
-**gen_listfromfile(const char *filename);
-static char
 **excl_list(const char *prname);
+static char
+**getfromfile(oper_t *ops);
 static void
-proc_normaldirs(ops_t *operation);
+processlist(char **synclist, oper_t *ops, int dotsornot);
 static char
-*target_from_option(char *tname);
-static char
-*target_from_file(void);
-
+*build_path(char *s1, char *s2, char *s3);
 
 int main(int argc, char **argv)
 {
 	options_t opts = process_options(argc, argv);	// options
-	ops_t operation = {0};	// preparation
-	check_args(&operation, argv, optind, &opts);
-	operation.timestamp = timestamp_name();
-	operation.excludes = excl_list("csmanager");
-	operation.files_list = make_dirslist(&operation); // data gathering.
-	if (operation.do_dot_dirs) {	// processing
-		process_dotdirs(&operation);
-	} else { /* possibly replace a sub-dir name with full path. */
-		if (opts.cloud_target) operation.cloud_target =
-								target_from_option(opts.cloud_target);
-		else operation.cloud_target = target_from_file();
-		proc_normaldirs(&operation);
+	char *srcdir = check_args(argv);
+	oper_t *operations = init_operations(srcdir, &opts);
+	char **synclist;
+	if (operations->filname) { // work from list of dirs given.
+		synclist = getfromfile(operations);
+		processlist(synclist, operations, 0);
+	} else { // work from source dir.
+		char **exlist = excl_list("csmanager");
+		printf("%s\n", "====================");
+		synclist = gen_dirslist(operations->dirname, 0, exlist);
+		processlist(synclist, operations, 0);
+		printf("%s\n", "====================");
+		synclist = gen_dirslist(operations->dirname, 1, exlist);
+		processlist(synclist, operations, 1);
+		printf("%s\n", "====================");
 	}
-	// and now deal with sundry files in $HOME or chosen dir.
-	gen_fileslist(operation.dirname, NULL);
+
+
 	return 0;
 }//main()
 
-void check_args(ops_t *operation, char **argv, int optind,
-			options_t *opts)
-{ /* Check presence of and validity of non-option args */
-	char buf[PATH_MAX] = {0};
-	if (opts->dirs_from && argv[optind]) {
-		sprintf(buf, "List file: %s and master dir %s not allowed\n",
-				opts->dirs_from, argv[optind]);
-	} else if (!argv[optind]) {
-		strcpy(buf, "No directory to process provided.\n");
-	} else if (!exists_dir(argv[optind])) {
-		sprintf(buf, "%s does not exist or is not a dir.\n",
-				argv[optind]);
-	} else if (opts->dirs_from && opts->do_dot_files) {
-		sprintf(buf, "May not process dot dirs in list of dirs: %s\n",
-				opts->dirs_from);
-	} else if (opts->dirs_from && !exists_file(opts->dirs_from)) {
-				sprintf(buf, "%s does not exist or is not a file.\n",
-						opts->dirs_from);
-	}
-
-	if (strlen(buf)) {
-		fprintf(stderr, "%s", buf);
-		exit(EXIT_FAILURE);
-	}
-	// Can complete ops_t struct
-	if (argv[optind]) {
-		operation->do_master_dir = 1;
-		operation->dirname = realpath(argv[optind], NULL);
-		if (!operation->dirname) {
-			perror(argv[optind]);
-			exit(EXIT_FAILURE);
-		}
-	} else if (opts->dirs_from) {
-		operation->dirname = xstrdup(opts->dirs_from);
-	}
-	operation->do_dot_dirs = opts->do_dot_files;
-	if (opts->dot_files_dir) {
-		// dot file location is initialised needed or not.
-		operation->dotfiles_dir = realpath(opts->dot_files_dir, NULL);
-		if (!operation->dotfiles_dir) {
-			perror(argv[optind]);
-			exit(EXIT_FAILURE);
-		}
-	} else { // dot dir tarballs go to standard place.
-		sprintf(buf, "%s/dotty", getenv("HOME"));
-		operation->dotfiles_dir = xstrdup(buf);
-	}
-	// set up the cloud target name
-	if (opts->cloud_target)
-	{ /* if user has optioned a target it won't be NULL */
-		operation->cloud_target = target_from_option(opts->cloud_target);
-	} else
-	{ /* no optional target so get it from file. */
-		operation->cloud_target = target_from_file();
-	}
-} // check_args()
-
 char
-**make_dirslist(ops_t *operation)
-{/* return a list of dirs using method depending on type of data given.
-**/
-	if (operation->do_master_dir) {
-		return gen_dirslist(operation->dirname, operation->do_dot_dirs,
-							operation->excludes);
-	} else {
-		return gen_listfromfile(operation->dirname);
+*check_args(char **argv)
+{ /* Check validity of non-option arg */
+	char buf[PATH_MAX] = {0};
+	char *p;
+	if (!argv[optind]) { // default to $HOME
+		p = realpath(getenv("HOME"), NULL);
+		strcpy(buf, p);
+	} else if ((p = realpath(argv[optind], NULL))) {
+		if (!p) { // must exist
+			perror(p);
+			exit(EXIT_FAILURE);
+		}
+		if (!exists_dir(p)) {
+			fprintf(stderr, "%s is not a dir.\n", argv[optind]);
+			exit(EXIT_FAILURE);
+		}
 	}
-} // make_dirslist()
+	return p;
+} // check_args()
 
 char
 **gen_dirslist(const char *dirname, int dotsornot, char **excl_list)
@@ -172,7 +113,7 @@ char
   * then turn the data into an array of C strings.
 */
 	mdata *md = init_mdata();
-	const size_t meminc = 1024 * 1024;	// This instance 1 meg good.
+	const size_t meminc = 1024 * 1024;	// 1 meg is ok for this job.
 	DIR *thedir = dopendir(dirname);
 	struct dirent *de;
 	while ((de = readdir(thedir))) {
@@ -197,102 +138,13 @@ char
 } // gen_dirslist()
 
 char
-**gen_listfromfile(const char *filename)
-{/* get the dir names contained in the named file,
-  * then turn the data into an array of C strings.
-*/
-	mdata *md = readfile(filename, 1, 0);	// terminated with '\n'
-	memlinestostr(md);	// C strings
-	char **result = memblocktoarray(md, 0);
-	free_mdata(md);
-	return result;
-} // gen_listfromfile()
-
-void
-process_dotdirs(ops_t *operation)
-{/* set up to recurse dot dirs */
-	if (!exists_file(operation->timestamp)) {
-		char command[PATH_MAX];
-		sprintf(command, "touch -d 1970-01-01 %s",
-					operation->timestamp);
-		(void)xsystem(command, 1);
-	}
-	operation->dd_updated = getfile_mtime(operation->timestamp);
-	if (!exists_dir(operation->dotfiles_dir))
-		newdir(operation->dotfiles_dir, 0);
-	size_t hl = home_len();
-	size_t i;
-	for (i = 0; operation->files_list[i] ; i++) {
-		// ->files_list is NULL terminated.
-		int res = nc_recursedir(operation, operation->files_list[i]);
-		if (res) {
-			char toname[NAME_MAX];
-			strcpy(toname, operation->dotfiles_dir);	// tarballs dir.
-			strjoin(toname, '/', operation->files_list[i] + hl+2,
-						NAME_MAX);
-			strjoin(toname, '.', "tgz", NAME_MAX);
-			/* tarball name is the name of the dot dir less '.'. */
-			char comm[PATH_MAX];
-			sprintf(comm, "tar -czf %s %s", toname,
-						operation->files_list[i]);
-			(void)xsystem(comm, 0);
-		}
-	}
-	char command[PATH_MAX];	// time stamp update
-	sprintf(command, "touch %s", operation->timestamp);
-	(void)xsystem(command, 1);
-} // process_dotdirs()
-
-char
-*timestamp_name(void)
-{
-	char buf[PATH_MAX];
-	strcpy(buf, getenv("HOME"));
-	strjoin(buf, '/', "dottim", PATH_MAX);
-	return xstrdup(buf);
-} // timestamp_name()
-
-int
-nc_recursedir(ops_t *operation, const char *dirname)
-{ /* Recurse the given dirname checking dir and file objects for
-   * age against the time stamp file. Return 1 if a newer object is
-   * found, 0 otherwise.
-*/
-	time_t stamp = getfile_mtime(dirname);
-	if (stamp > operation->dd_updated) return 1;
-	DIR *dp = dopendir(dirname);
-	struct dirent *de;
-	while ((de = readdir(dp))) {
-		if (strcmp(de->d_name, ".") == 0 ) continue;
-		if (strcmp(de->d_name, "..") == 0) continue;
-		if (!(de->d_type == DT_REG || de->d_type == DT_DIR)) continue;
-		char joinbuf[PATH_MAX];
-		strcpy(joinbuf, dirname);
-		strjoin(joinbuf, '/', de->d_name, PATH_MAX);
-		stamp = getfile_mtime(joinbuf);
-		if (stamp > operation->dd_updated){
-			doclosedir(dp);
-			return 1;
-		}
-		if (de->d_type == DT_DIR) {
-			int result = nc_recursedir(operation, joinbuf);
-			if (result) {
-				doclosedir(dp);
-				return 1;
-			}
-		}
-	} // while()
-	doclosedir(dp);
-	return 0;
-} // nc_recursedir()
-
-char
 **excl_list(const char *prname)
 {/* Return list of dirs to exclude from processing. If the excludes file
   * does not exist, create it with some reasonable default values.
 */
 	char fpath[PATH_MAX] = {0};
-	sprintf(fpath, "%s/.config/%s/excl.lst", getenv("HOME"), prname);
+	char *home = getenv("HOME");
+	sprintf(fpath, "%s/.config/%s/excl.lst", home, prname);
 	if (!exists_file(fpath))
 	{	// create it
 		char *cp = strstr(fpath, "excl.lst");
@@ -300,94 +152,145 @@ char
 		if (!exists_dir(fpath)) newdir(fpath, 0);
 		*cp = 'e';	// restore the filename to fpath.
 		FILE *fpo = dofopen(fpath, "w");
-		fprintf(fpo, "%s/%s\n", getenv("HOME"), "Dropbox");
-		fprintf(fpo, "%s/%s\n", getenv("HOME"), "Nextcloud");
+		fprintf(fpo, "%s/%s\n", home, "Dropbox");
+		fprintf(fpo, "%s/%s\n", home, "Nextcloud");
 		dofclose(fpo);
 		sync();
 	}
 	return getfile_str(fpath);
 } // excl_list()
 
-static void
-proc_normaldirs(ops_t *operation)
-{/* We have an array of C strings of the form $HOME/dirname. Required to
-  * hard link all the files and sub dirs under $HOME/dirname into
-  * $HOME/cloud_target/dirname, creating dirs in target as needed.
-*/
-	size_t hl = home_len();
-	size_t i;
-	for (i = 0; operation->files_list[i]; i++)
-	{	// it's a NULL terminated list
-		char *frdir = operation->files_list[i];
-		char todir[PATH_MAX];
-		strcpy(todir, operation->cloud_target);
-		strjoin(todir, '/', &frdir[hl+1], PATH_MAX);
-		if(!exists_dir(todir)) newdir(todir, 1);
-		char command[PATH_MAX];
-		sprintf(command, "synclink '%s' '%s'", frdir, todir);
-		int res = xsystem(command, 0);
-		if (res)
-		{
-			fprintf(stderr, "Non zero system result: %d\n", res);
+oper_t
+*init_operations(char *srcdir, options_t *opts)
+{ /* setup the variables to be operated on thru the run. */
+	oper_t *operations = xmalloc(sizeof (oper_t));
+	memset(operations, 0, sizeof(oper_t));
+	// need to change into srcdir, by default maybe $HOME
+	int res = chdir(srcdir);
+	if (res == -1) {
+		perror(srcdir);
+		exit(EXIT_FAILURE);
+	}
+	operations->dirname = srcdir;
+	// Need to deal with target before anything else.
+	size_t len2target;
+	char wrkbuf[PATH_MAX];
+	char *stop;
+	if (opts->cloud_target) { // it's on the heap.
+		operations->cloud_target =
+				build_path(srcdir, opts->cloud_target, NULL);
+		strcpy(wrkbuf, operations->cloud_target);
+		stop = strstr(wrkbuf, opts->cloud_target);
+		free(opts->cloud_target);
+	} else { // use the default.
+		operations->cloud_target =
+						build_path(srcdir, "Nextcloud", NULL);
+		strcpy(wrkbuf, operations->cloud_target);
+		stop = strstr(wrkbuf, "Nextcloud");
+	}
+	*stop = '\0';
+	len2target = strlen(wrkbuf) - 1;	// backup to '/' before target.
+	operations->len2target = len2target;
+	// Now the dotfiles dir.
+	if (opts->dot_files_dir) {
+		operations->dotdirs_dir =
+		build_path(operations->cloud_target, opts->dot_files_dir, NULL);
+		free(opts->dot_files_dir);	// on the heap from options proc.
+	} else {
+		operations->dotdirs_dir =
+				build_path(operations->cloud_target, "Dotty", NULL);
+	}
+	// deal with the source.
+	if (opts->dirs_from) {
+		operations->filname = build_path(srcdir, opts->dirs_from, NULL);
+		if (!exists_file(operations->filname)) {
+			fprintf(stderr, "No such file: %s\n", operations->filname);
+			exit(EXIT_FAILURE);
 		}
 	}
-} // proc_normaldirs()
+	return operations;
+} // init_operations()
 
 char
-*target_from_option(char *tname)
-{	/* set up the cloud directory name from tname */
-	char joinbuf[PATH_MAX];
-	char *cp = get_home();
-	strcpy(joinbuf, cp);
-	free(cp);
-	strjoin(joinbuf, '/', tname, PATH_MAX);
-	return xstrdup(joinbuf);
-} // target_from_option()
-
-char
-*target_from_file(void)
-{	/* set up the cloud directory name from config file */
-	char *cfgn = getcfgfn("csmanager", "target");
-	char *tmp;
-	if (!exists_file(cfgn))
-	{
-		str2file(cfgn, "Nextcloud");
-		tmp = xstrdup("Nextcloud");
-	} else
-	{
-		mdata *md = readfile(cfgn, 0, 1);
-		memlinestostr(md);
-		tmp = xstrdup(md->fro);
-		free_mdata(md);
+**getfromfile(oper_t *ops)
+{ /* Read the given file and generate the list of dirs from it. */
+	mdata *mydat = readfile(ops->filname, 1, 0);
+	size_t count = memlinestostr(mydat);
+	char **list = xmalloc((count+1) * sizeof(char *));
+	list[count] = (char *)NULL;
+	char *cp = mydat->fro;
+	size_t i;
+	for (i = 0; i < count; i++) {
+		if (cp[0] == '/') { // absolute path specified.
+			list[i] = xstrdup(cp);
+		} else {
+			char line[PATH_MAX];
+		/* dirs named in file must be relative to named source dir. */
+			strcpy(line, ops->dirname);
+			strjoin(line, '/', cp, PATH_MAX);
+			list[i] = xstrdup(line);
+		}
+		if (!exists_dir(list[i])) {
+			fprintf(stderr, "No such dir: %s\n", list[i]);
+			exit(EXIT_FAILURE);
+		}
+		cp += strlen(cp) + 1;
 	}
-	free(cfgn);
-	char buf[PATH_MAX];
-	sprintf(buf, "%s/%s", getenv("HOME"), tmp);
-	free(tmp);
-	return xstrdup(buf);
-} // target_from_file()
+	free(mydat->fro);
+	return list;
+} // getfromfile()
 
-char
-**gen_fileslist(const char *dirname, char **excl_list)
-{/* get the file names under dirname, then turn the data into an array
-  * of C strings.
+void
+processlist(char **synclist, oper_t *ops, int dotsornot)
+{ /* From the list of absolute paths in synclist, sync to the cloud
+   * target.
 */
-	mdata *md = init_mdata();
-	const size_t meminc = PATH_MAX;	// This instance 4k meg good.
-	DIR *thedir = dopendir(dirname);
-	struct dirent *de;
-	while ((de = readdir(thedir))) {
-		if(strcmp("..", de->d_name) == 0) continue;
-		if(strcmp(".", de->d_name) == 0) continue;
-		if(de->d_type != DT_REG) continue;
+	size_t i;
+	size_t hlen = ops->len2target;
+	for (i = 0; synclist[i]; i++) {
 		char buf[PATH_MAX];
-		strcpy(buf, dirname);
-		strjoin(buf, '/', de->d_name, PATH_MAX);
-		if(inlist(buf, excl_list)) continue;
-		meminsert(buf, md, meminc);
+		char command[PATH_MAX];
+		strcpy(buf, synclist[i]);
+		buf[hlen] = 0;
+		strcpy(buf, ops->cloud_target);
+		if (dotsornot) {
+			strcpy(buf, ops->dotdirs_dir);
+			if (!exists_dir(buf)) {
+				int res = mkdir(buf, 0775);
+				if (res) {
+					perror(buf);
+					exit(EXIT_FAILURE);
+				}
+			}
+		}
+		// For every $HOME/somedir, create $HOME/Nextcloud/somedir
+		char wrk[PATH_MAX];
+		strcpy(wrk, synclist[i]);
+		char *cp = &wrk[hlen+1];
+		strjoin(buf, '/', cp, PATH_MAX);
+		if (!exists_dir(buf)) {
+			int res = mkdir(buf, 0775);
+			if (res) {
+				perror(buf);
+				exit(EXIT_FAILURE);
+			}
+		}
+		strcpy(command, "/usr/local/bin/synclink -v");
+		strjoin(command, ' ', synclist[i], PATH_MAX);
+		strjoin(command, ' ', buf, PATH_MAX);
+		printf("%s\n", command);
+		xsystem(command, 0);
 	}
-	doclosedir(thedir);
-	char **result = memblocktoarray(md, 0);
-	free_mdata(md);
-	return result;
-} // gen_fileslist()
+} // processlist()
+
+char
+*build_path(char *s1, char *s2, char *s3)
+{ /* Assemble a path of names separated by '/', s3 may be NULL. */
+	char buf[PATH_MAX];
+	strcpy(buf, s1);
+	strjoin(buf, '/', s2, PATH_MAX);
+	if (s3) {
+		strjoin(buf, '/', s3, PATH_MAX);
+	}
+	return xstrdup(buf);
+} // build_path()
